05c2e3c6cba94f5b7b2c43ce3b49385920444566
yukai chen <chenyukai@yukais-MacBook-Pro.local>
2024-06-22 21:08:20 -0600

    update port number as AWS only allow 443 for https request

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,20 @@ import express from 'express';
 import session from 'express-session';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import https from 'https';
+import fs from 'fs';
 import 'isomorphic-fetch';
 import submitRoutes from './submit.js';
 
 
 const APP = express();
 const PORT = process.env.REACT_APP_BACKEND_PORT || 3001;
+
+// SSL/TLS certificates
+const credentials = { 
+    key: fs.readFileSync('cert/server.key', 'utf8'), 
+    cert: fs.readFileSync('cert/server.cert', 'utf8') 
+};
 
 APP.use(session({
     secret: 'my_secret',
@@ -21,7 +29,8 @@ APP.use(bodyParser.json());
 APP.get('/', (req, res) => res.json(`My API running at port ${PORT}...`));
 APP.use('/', submitRoutes);
 
+const httpsServer = https.createServer(credentials, APP);
 
-APP.listen(PORT, () => {
-    console.log(`Server running at http://0.0.0.0:${PORT}`);
+httpsServer.listen(PORT, () => {
+    console.log(`Server running at https://0.0.0.0:${PORT}`);
 });
